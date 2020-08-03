4524973e40343e38b6a9e2bb76bb6e8a72883017
mplesser <mlesser@arizona.edu>
2020-08-03 10:38:07 -0700

    cleanup

--- start_monitor.bat
+++ start_monitor.bat
@@ -1,5 +1,5 @@
 @echo off
 
-rem python C:\data\code\azcammonitor\.py -configfile "/data/code/azcam-vatt/bin/parameters_vatt_monitor.ini"
+rem An example...
 
 python C:\azcam\azcammonitor\azcammonitor.py -configfile "/azcam/azcam-vatt/bin/parameters_vatt_monitor.ini"
